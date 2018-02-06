2c33558bfd1e3d4df42e79ef6fbb37efa9cebaba
lidj <>
2018-02-06 09:28:08 +0000

    Integrated HockeyApp SDK for IOS

---
+++ ios/Pods/Target Support Files/Pods-RegiaApp/Pods-RegiaApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RegiaApp : NSObject
+@end
+@implementation PodsDummy_Pods_RegiaApp
+@end
--- ios/RegiaApp/AppDelegate.m
+++ ios/RegiaApp/AppDelegate.m
@@ -18,7 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+//jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RegiaApp"
@@ -33,5 +34,6 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
+
 
 @end
