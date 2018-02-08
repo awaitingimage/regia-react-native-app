cfd5b4671734d1d23b7b65cafc00aab257369e78
lidj <>
2018-02-08 20:11:17 +0000

    Added privateconfig file that is on the gitignore file

--- App/Containers/RootContainer/RootContainer.tsx
+++ App/Containers/RootContainer/RootContainer.tsx
@@ -5,11 +5,10 @@ import ReduxPersist from "../../Config/ReduxPersist";
 import ReduxNavigation from "../../Navigation/ReduxNavigation";
 import { StartupActions } from "../../Reducers/StartupReducers";
 import HockeyApp from "react-native-hockeyapp";
+import PrivateConfig from "../../Config/PrivateConfig";
 
 // Styles
 import styles from "./RootContainerStyles";
-
-const HOCKEY_APP_ID = "bddaee14e1cd4f0cafed78b295de040d";
 
 interface Props {
   startup: () => void;
@@ -22,14 +21,16 @@ interface State {
 export class RootContainer extends React.Component<Props, State> {
   public componentWillMount() {
     if (Platform.OS === "android") {
-      HockeyApp.configure(HOCKEY_APP_ID, true);
+      HockeyApp.configure(PrivateConfig.hockeyAppIdAndroid, true);
+    }
+    if (Platform.OS === "ios") {
+      HockeyApp.configure(PrivateConfig.hockeyAppIdIOS, true);
     }
   }
   public componentDidMount() {
-    if (Platform.OS === "android") {
+
       HockeyApp.start();
       HockeyApp.checkForUpdate();
-    }
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
       this.props.startup();
