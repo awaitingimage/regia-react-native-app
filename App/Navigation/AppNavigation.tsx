d360799cb38681bcafd4fb566e225843d91552ed
awaitingimage <liam@awaitingimage.com>
2018-04-04 10:03:31 +0100

    Set navigation route back to events

--- App/Navigation/AppNavigation.tsx
+++ App/Navigation/AppNavigation.tsx
@@ -30,7 +30,7 @@ const RootDrawer = DrawerNavigator({
   drawerPosition: "left",
   contentComponent: DrawerMenu,
   drawerWidth: Metrics.screenWidth - 65,
-  initialRouteName: "AboutScreen",
+  initialRouteName: "EventsScreen",
 });
 
 export default RootDrawer;
